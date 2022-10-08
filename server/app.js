8b65cdc175fa9b5757b621dad9f3ade7dae61ed9
Hridoy <rakibhasanhridoy776@gmail.com>
2022-10-08 15:30:43 +0600

    error route and / route added in server

--- server/app.js
+++ server/app.js
@@ -12,7 +12,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    res.send("<h2>this is homepage</h2>")
+})
 
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
 
 
 
